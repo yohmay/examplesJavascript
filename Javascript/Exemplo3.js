2929c2621f6e1bc681c0531203c98c027be32d09
Yohana Mayra <yohanamayra@gmail.com>
2023-06-07 22:48:59 -0300

    Exemplos em JavaScript

---
+++ Javascript/Exemplo3.js
@@ -0,0 +1,8 @@
+let numero1 = 10
+let numero2 = 11;
+
+if (numero1 == numero2) {
+    console.log("Os números são iguais.");
+} else {
+    console.log("Os números são diferentes.");
+}
---
+++ Javascript/Exemplo6.js
@@ -0,0 +1,7 @@
+let primeiro = 5;
+let segundo = 10;
+
+let soma = (parseFloat(primeiro) + parseFloat(segundo));
+let resultado = soma;
+
+console.log("O resultado é de: " + resultado);
